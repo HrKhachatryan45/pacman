939f55a8941558ba49055c6b7cbf01c4939a7e74
Your Name <you@example.com>
2025-06-03 14:50:28 +0400

    changed host

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const requestRoutes = require('./routes/requestRoutes');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 app.use(cookieParser());
 app.use(cors())
 app.use(express.json());

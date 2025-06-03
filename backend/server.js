ae896e0445ddb46a5a6da2a19915fedd628520fb
Your Name <you@example.com>
2025-06-03 14:46:14 +0400

    added forgotten line

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,21 @@ app.use(cors())
 app.use(express.json());
 app.use('/requests',requestRoutes)
 app.use('/auth',authRoutes)
+
+
+const __name = path.resolve()
+
+app.use(express.static(path.join(__name, "frontend", "build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__name,"frontend","build","index.html"));
+})
+
+
+
 const PORT = process.env.PORT || 8080;
+
+
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
